0de45fb452c75dcc1ce9b1467f8ec0d61985279e
Maksi24816 <93403660+Maksi24816@users.noreply.github.com>
2022-12-30 19:56:23 +0200

    0.1.6

---
+++ JS/Script.js
@@ -0,0 +1,193 @@
+$(document).ready(function() {
+	$('.Xr').click(function(event) {
+		$('.boxI,.inf,.Inform').addClass('ActXr');
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.LustBox').click(function(event) {
+		$('.Levo,.LustBox,.Lust,.PL,.Inform').toggleClass('Act1');
+	});
+});
+$(document).ready(function() {
+	$('.SestBox').click(function(event) {
+		$('.Pravo,.SestBox,.Sest,.PL,.Inform').toggleClass('Act2');
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.M').click(function(event) {
+		$('.r154,.C310,.C320').toggleClass('ActM');
+	});
+});
+$(document).ready(function() {
+	$('.M').click(function(event) {
+		$('.R1wh,.r1,.M').toggleClass('r1pk');
+	});
+});
+$(document).ready(function() {
+	$('.M').click(function(event) {
+		$('.R2wh').toggleClass('r2pk');
+	});
+});
+$(document).ready(function() {
+	$('.M').click(function(event) {
+		$('.r34').toggleClass('r34A');
+	});
+});
+$(document).ready(function() {
+	$('.M').click(function(event) {
+		$('.K21,.K22,.K23,.K24,.K25').toggleClass('K0');
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.K24').click(function(event) {
+		$('.r154').toggleClass('r154p');
+	});
+});
+
+
+$(document).ready(function() {
+	$('.K21').click(function(event) {
+		$('.C21,.C22,.C23,.C24,.C25').toggleClass('Act2K21');
+	});
+});
+$(document).ready(function() {
+	$('.K22').click(function(event) {
+		$('.C21,.C22,.C23,.C24,.C25').toggleClass('Act2K22');
+	});
+});
+$(document).ready(function() {
+	$('.K23').click(function(event) {
+		$('.C21,.C22,.C23,.C24,.C25').toggleClass('Act2K23');
+	});
+});
+$(document).ready(function() {
+	$('.K24').click(function(event) {
+		$('.C21,.C22,.C23,.C24,.C25').toggleClass('Act2K24');
+	});
+});
+$(document).ready(function() {
+	$('.K25').click(function(event) {
+		$('.C21,.C22,.C23,.C24,.C25').toggleClass('Act2K25');
+	});
+});
+
+
+$(document).ready(function() {
+	$('.K21').click(function(event) {
+		$('.K22,.K23,.K24,.K25').toggleClass('K0');
+	});
+});
+$(document).ready(function() {
+	$('.K21').click(function(event) {
+		$('.K21').toggleClass('K21K1');
+	});
+});
+$(document).ready(function() {
+	$('.K22').click(function(event) {
+		$('.K21,.K23,.K24,.K25').toggleClass('K0');
+	});
+});
+$(document).ready(function() {
+	$('.K22').click(function(event) {
+		$('.K22').toggleClass('K22K2');
+	});
+});
+$(document).ready(function() {
+	$('.K23').click(function(event) {
+		$('.K21,.K22,.K24,.K25').toggleClass('K0');
+	});
+});
+$(document).ready(function() {
+	$('.K24').click(function(event) {
+		$('.K21,.K22,.K23,.K25').toggleClass('K0');
+	});
+});
+$(document).ready(function() {
+	$('.K24').click(function(event) {
+		$('.K24').toggleClass('K24K4');
+	});
+});
+$(document).ready(function() {
+	$('.K25').click(function(event) {
+		$('.K21,.K22,.K23,.K24').toggleClass('K0');
+	});
+});
+$(document).ready(function() {
+	$('.K25').click(function(event) {
+		$('.K25').toggleClass('K25K5');
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.K21,.K22,.K23,.K24,.K25').click(function(event) {
+		$('.C310').toggleClass('Act310');
+	});
+});
+$(document).ready(function() {
+	$('.K21,.K22,.K23,.K24,.K25').click(function(event) {
+		$('.C320').toggleClass('Act320');
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.K21').click(function(event) {
+		$('.ID31').toggleClass('Act');
+	});
+});
+$(document).ready(function() {
+	$('.K22').click(function(event) {
+		$('.ID32').toggleClass('Act');
+	});
+});
+$(document).ready(function() {
+	$('.K23').click(function(event) {
+		$('.ID33').toggleClass('Act');
+	});
+});
+$(document).ready(function() {
+	$('.K24').click(function(event) {
+		$('.ID34').toggleClass('Act');
+	});
+});
+$(document).ready(function() {
+	$('.K25').click(function(event) {
+		$('.ID35').toggleClass('Act');
+	});
+});
+
+
+$(document).ready(function() {
+	$('.IBoxTypeCurrentlyFotoBoxDop').hover(function(event) {
+		$('.IBoxTypeCurrentlyFotoBoxDopStrelkaLiniya1,.IBoxTypeCurrentlyFotoBoxDopStrelkaLiniya2').toggleClass('Act');
+	});
+});
+
+$(document).ready(function() {
+	$('.IBoxTypeCurrentlyFotoBoxDop').click(function(event) {
+		$('.IBoxTypeCurrentlyFotoBoxDop,.IBoxTypeCurrentlyFotoBoxDopStrelka1,.IBoxTypeCurrentlyFotoBoxDopStrelka2,.IBoxTypeCurrentlyFotoBoxDopStrelka3').toggleClass('Act');
+	});
+});
+
+$(document).ready(function() {
+	$('.TypeFoto').click(function(event) {
+		$('.TypeFoto,.KoloHeader').toggleClass('Act');
+	});
+});
+
+
+
+
+
