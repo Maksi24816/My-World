4b02f4312438b855c1067c328a94d0573bb21e2e
Maksi24816 <93403660+Maksi24816@users.noreply.github.com>
2023-02-03 17:18:06 +0200

    Add files via upload

--- JS/Info_Type.js
+++ JS/Info_Type.js
@@ -8,21 +8,27 @@ const BlockCenter = document.querySelector('.Body_Type2_BlockB1_Center');
 const BlockRight = document.querySelector('.Body_Type2_BlockB1_Right');
 
 Pynkt1.onclick = function () {
-	BlockLeft.classList.add('Act');
-	BlockCenter.classList.add('Act');
-	BlockRight.classList.remove('Act');
+	BlockLeft.style.left = "-40%";
+	BlockCenter.style.top = "500px";
+	BlockCenter.style.opacity = "0";
+	BlockCenter.style.visibility = "hidden";
+	BlockRight.style.right = "-400%";
 }
 
 Pynkt2.onclick = function () {
-	BlockLeft.classList.remove('Act');
-	BlockCenter.classList.remove('Act');
-	BlockRight.classList.remove('Act');
+	BlockLeft.style.left = "-400%";
+	BlockCenter.style.top = "126px";
+	BlockCenter.style.opacity = "1";
+	BlockCenter.style.visibility = "visible";
+	BlockRight.style.right = "-400%";
 }
 
 Pynkt3.onclick = function () {
-	BlockLeft.classList.remove('Act');
-	BlockCenter.classList.add('Act');
-	BlockRight.classList.add('Act');
+	BlockLeft.style.left = "-400%";
+	BlockCenter.style.top = "500px";
+	BlockCenter.style.opacity = "0";
+	BlockCenter.style.visibility = "hidden";
+	BlockRight.style.right = "-40%";
 }
 
 const Knopka = document.querySelectorAll('.Type3_Element_Knopka');
